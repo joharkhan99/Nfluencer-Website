d82939900be9d6b25766e5047317f1870afe4b0d
single-byte <70197335+single-byte@users.noreply.github.com>
2023-05-15 15:48:35 +0500

    added tailwind

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./nftmarketplace/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/marketplace" element={Home} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ client/src/nftmarketplace/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/style.css";
+
+export default function Home() {
+  return (
+    <div>
+      Home
+      <h1>This is MArkeplace home</h1>
+    </div>
+  );
+}
