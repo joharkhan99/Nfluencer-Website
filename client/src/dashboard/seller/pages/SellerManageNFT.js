99b0f3f945558888ca05de14135b3a615317fc39
single-byte <joharkhan1999@gmail.com>
2023-11-11 18:13:08 +0500

    restructured the nft page

--- client/src/dashboard/seller/pages/SellerManageNFT.js
+++ client/src/dashboard/seller/pages/SellerManageNFT.js
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setWalletAddress } from "../../../redux/slices/UserSlice";
 import TrendingNFTs from "../components/nft/TrendingNFTs";
 import TopCreators from "../components/nft/TopCreators";
+import FirstNFT from "../components/nft/FirstNFT";
 
 const SellerManageNFT = () => {
   const [haveMetamask, sethaveMetamask] = useState(false);
@@ -147,17 +148,6 @@ const SellerManageNFT = () => {
   useEffect(() => {
     connectedAccounts();
   });
-
-  const formatDate = (dateString) => {
-    var date = new Date(dateString);
-    return (
-      date.getDate() +
-      " " +
-      date.toLocaleString("default", { month: "long" }) +
-      " " +
-      date.getFullYear()
-    );
-  };
 
   return (
     <div className="w-full">
@@ -358,89 +348,7 @@ const SellerManageNFT = () => {
             Your NFTs
           </h2>
 
-          {firstNFT && (
-            <div className="w-full flex bg-white p-5 rounded-2xl gap-10">
-              <div className="w-1/3">
-                <div>
-                  <img
-                    src={firstNFT.image}
-                    alt=""
-                    className="w-full h-96 object-cover rounded-2xl"
-                  />
-                </div>
-              </div>
-              <div className="w-2/3">
-                <div className="flex flex-col gap-3 text-gray-800">
-                  <h3 className="font-bold text-3xl">{firstNFT.name}</h3>
-                  <p className="text-gray-500 text-sm">
-                    Published on {formatDate(firstNFT.createdAt)}
-                  </p>
-
-                  <div className="flex items-center justify-start gap-5">
-                    <img
-                      src={firstNFT.user.avatar}
-                      alt=""
-                      className="w-14 h-14 rounded-full"
-                    />
-                    <div className="flex flex-col">
-                      <span className="font-bold">{firstNFT.user.name}</span>
-                      <span className="text-gray-500 font-medium">
-                        @{firstNFT.user.username}
-                      </span>
-                    </div>
-                  </div>
-
-                  <p className="py-4 block">
-                    {firstNFT.description.substring(0, 200)}...
-                  </p>
-
-                  <div className="bg-gray-100 w-fit flex p-8 px-12 rounded-xl gap-14">
-                    <div className="flex flex-col gap-2">
-                      <span className="text-sm font-semibold text-gray-500">
-                        Price Starts From
-                      </span>
-                      <span className="flex gap-2">
-                        <img
-                          src={require("../../../nftmarketplace/assets/eth.png")}
-                          alt=""
-                          className="w-8 h-8 object-contain"
-                        />
-                        <span className="font-bold text-xl">
-                          {firstNFT.price} ETH
-                        </span>
-                      </span>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <span className="text-sm font-semibold text-gray-500">
-                        Creator Royalty
-                      </span>
-                      <span className="flex gap-2 items-center">
-                        <TrophyIcon className="w-8 h-8 object-contain text-yellow-500" />
-                        <span className="font-bold text-xl">
-                          {firstNFT.royalties} %
-                        </span>
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="flex items-center gap-10 mt-2">
-                    <button className="rounded-full text-nft-primary-light bg-purple-200 p-2">
-                      <HeartIcon className="h-6 w-6 fill-nft-primary-light" />
-                    </button>
-
-                    <div className="flex items-center gap-4">
-                      <button className="bg-gray-200 border border-gray-300 font-bold p-3 rounded-xl px-10 hover:opacity-80">
-                        Details
-                      </button>
-                      <button className="bg-nft-primary-light border border-nft-primary-light text-white font-bold p-3 rounded-xl px-10 hover:opacity-80">
-                        Place a Bid
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
+          {firstNFT && <FirstNFT NFT={firstNFT} />}
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-10">
             {nfts.map((nft) => (
